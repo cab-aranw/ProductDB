7b1deb8e9307405919af5af2a851d5bba4fc7baa
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2020-08-13 22:46:51 -0400

    1.1

--- ProductDB/Program.cs
+++ ProductDB/Program.cs
@@ -16,7 +16,7 @@ namespace ProductDB
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
